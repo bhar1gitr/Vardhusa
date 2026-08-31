cbfe4ebe1c4f82f0d300e4b8b9162e66fe40e681
@devpragmatic! <dev.1@pragmaticintelligence.com>
2026-08-31 10:33:07 +0530

    added new projects

---
+++ src/data/projectsData.js
@@ -0,0 +1,214 @@
+import project1 from "../assets/projects/1 LEH Airport.png";
+import project2 from "../assets/projects/2 Conscient One.jpg";
+import project3 from "../assets/projects/3 Dhoot Time Arcade 1.jpg";
+import project4 from "../assets/projects/4 Indospace Warehouse.jpeg";
+import project5 from "../assets/projects/5 schott kaisha.jpeg";
+import project6 from "../assets/projects/6 Godrej Boulevard.jpeg";
+import project7 from "../assets/projects/7 Golf Green.jpeg";
+import project8 from "../assets/projects/8 Experion Capital.jpg";
+import project9 from "../assets/projects/Aisat Jewar Aiprport.jpeg";
+import project10 from "../assets/projects/BDD Worli.jpeg";
+import project11 from "../assets/projects/Birla Navya.jpeg";
+import project12 from "../assets/projects/Experio.jpeg";
+import project13 from "../assets/projects/Habitat Prime.jpeg";
+import project14 from "../assets/projects/Natura 1Oak.jpeg";
+import project15 from "../assets/projects/TCG Pune.jpeg";
+import project16 from "../assets/projects/TCS Pune.jpeg";
+import p1 from "../assets/projects/1.jpeg";
+import p2 from "../assets/projects/2.jpeg";
+import p3 from "../assets/projects/3.jpeg";
+import p4 from "../assets/projects/4.jpeg";
+import p5 from "../assets/projects/5.jpeg";
+import p6 from "../assets/projects/6.jpeg";
+import p7 from "../assets/projects/7.jpeg";
+import p8 from "../assets/projects/8.jpeg";
+
+export const CATEGORIES = [
+  "All",
+  "Residential",
+  "Commercial",
+  "Institutional",
+  "Industrial",
+  "Infrastructure",
+  "Healthcare",
+  "Hospitality",
+  "Data Centres"
+];
+
+export const PROJECTS = [
+  {
+    name: "Leh Airport",
+    location: "Ladakh, India",
+    category: "Infrastructure",
+    scope: "Plumbing & Firefighting",
+    img: project1,
+  },
+  {
+    name: "Conscient One",
+    location: "Gurugram, Haryana",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: project2,
+  },
+  {
+    name: "Time Arcade",
+    location: "Gurugram, Haryana",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: project3,
+  },
+  {
+    name: "Indospace Badli",
+    location: "Badli, Haryana",
+    category: "Industrial",
+    scope: "Firefighting",
+    img: project4,
+  },
+  {
+    name: "Schott Kaisha",
+    location: "India",
+    category: "Industrial",
+    scope: "Plumbing & Firefighting",
+    img: project5,
+  },
+  {
+    name: "Godrej Boulevard",
+    location: "India",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project6,
+  },
+  {
+    name: "Golf Green",
+    location: "India",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project7,
+  },
+  {
+    name: "Experion Capital",
+    location: "India",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: project8,
+  },
+  {
+    name: "Aisat Jewar Airport",
+    location: "Jewar, UP",
+    category: "Industrial",
+    scope: "Plumbing & Firefighting",
+    img: project9,
+  },
+  {
+    name: "BDD Worli",
+    location: "Mumbai, Maharashtra",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project10,
+  },
+  {
+    name: "Birla Navya",
+    location: "Gurgaon, Haryana",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project11,
+  },
+  {
+    name: "Experio",
+    location: "Lucknow, UP",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: project12,
+  },
+  {
+    name: "Habitat Prime",
+    location: "Gurgaon, Haryana",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project13,
+  },
+  {
+    name: "Natura 1Oak",
+    location: "Lucknow, UP",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project14,
+  },
+  {
+    name: "TCG Pune",
+    location: "Pune, Maharashtra",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: project15,
+  },
+  {
+    name: "TCS Pune",
+    location: "Pune, Maharashtra",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: project16,
+  },
+  {
+    name: "Project Name 1",
+    location: "Location",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: p1,
+  },
+  {
+    name: "Project Name 2",
+    location: "Location",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: p2,
+  },
+  {
+    name: "Project Name 3",
+    location: "Location",
+    category: "Industrial",
+    scope: "Firefighting",
+    img: p3,
+  },
+  {
+    name: "Project Name 4",
+    location: "Location",
+    category: "Infrastructure",
+    scope: "Plumbing & Firefighting",
+    img: p4,
+  },
+  {
+    name: "Project Name 5",
+    location: "Location",
+    category: "Residential",
+    scope: "Plumbing & Firefighting",
+    img: p5,
+  },
+  {
+    name: "Project Name 6",
+    location: "Location",
+    category: "Commercial",
+    scope: "Plumbing & Firefighting",
+    img: p6,
+  },
+  {
+    name: "Project Name 7",
+    location: "Location",
+    category: "Industrial",
+    scope: "Plumbing & Firefighting",
+    img: p7,
+  },
+  {
+    name: "Project Name 8",
+    location: "Location",
+    category: "Infrastructure",
+    scope: "Plumbing & Firefighting",
+    img: p8,
+  },
+];
+
+export const STATS = [
+  { value: "25+", label: "Projects Delivered" },
+  { value: "8+", label: "Cities Covered" },
+  { value: "3", label: "Sectors Served" },
+  { value: "200+", label: "People on Site" },
+];
