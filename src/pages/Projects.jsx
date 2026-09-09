535bd8cdd63b1f27674f979a0a7d93e26163ee08
@devpragmatic! <dev.1@pragmaticintelligence.com>
2026-09-09 19:53:47 +0530

    changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -146,7 +146,7 @@ export default function Projects() {
                       text-[#858585]
                     "
                   >
-                    Our Portfolio
+                    Project Portfolio
                   </span>
                 </div>
 
